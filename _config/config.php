ea47383b745057bd51f0eb0ec26489d4e13b48a1
anmiefan <61576693+anmiefan@users.noreply.github.com>
2020-03-11 00:43:06 -0400

    Update config.php

--- _config/config.php
+++ _config/config.php
@@ -13,12 +13,12 @@ $_CONFIG['gl_pr_cr'] = '1561762265';
 $_CONFIG['gl_lzid'] = '200788e963';
 $_CONFIG['b64'] = false;
 
-$_CONFIG[0]['gl_db_host'] = 'localhost';
-$_CONFIG[0]['gl_db_user'] = 'root';
+$_CONFIG[0]['gl_db_host'] = 'sgcbff.sgcosirserver.tk';
+$_CONFIG[0]['gl_db_user'] = 'osi99';
 $_CONFIG[0]['gl_db_ext'] = 'mysqli';
 $_CONFIG[0]['gl_db_eng'] = 'MyISAM';
 $_CONFIG[0]['gl_db_pass'] = 'Sailormoon1@';
-$_CONFIG[0]['gl_db_name'] = 'livez8';
+$_CONFIG[0]['gl_db_name'] = 'bfflivezilla8';
 $_CONFIG[0]['gl_db_prefix'] = 'lz_';
 
 ?>
